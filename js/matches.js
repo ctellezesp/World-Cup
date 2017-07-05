67d4fc90c4feaa62aea47c975b726c27b29cd781
Carlos <A01274912@itesm.mx>
2017-07-04 23:41:50 -0500

    English Added

---
+++ js/matches.js
@@ -0,0 +1,185 @@
+function G1(){
+    document.getElementById("md1").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16183" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G2(){
+    document.getElementById("md2").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16261" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G3(){
+    document.getElementById("md3").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16263" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}    
+
+function G4(){
+    document.getElementById("md4").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16265" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G5(){
+    document.getElementById("md5").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16267" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G6(){
+    document.getElementById("md6").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16269" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}    
+
+function G7(){
+    document.getElementById("md7").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16271" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}  
+
+function G8(){
+    document.getElementById("md8").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16273" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}      
+
+function G9(){
+    document.getElementById("md9").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16275" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G10(){
+    document.getElementById("md10").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16277" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G11(){
+    document.getElementById("md11").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16279" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G12(){
+    document.getElementById("md12").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16281" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G13(){
+    document.getElementById("md13").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16283" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G14(){
+    document.getElementById("md14").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16285" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G15(){
+    document.getElementById("md15").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16287" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G16(){
+    document.getElementById("md16").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16289" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G17(){
+    document.getElementById("md17").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16291" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G18(){
+    document.getElementById("md18").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16327" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G19(){
+    document.getElementById("md19").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16329" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}    
+
+function G20(){
+    document.getElementById("md20").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16362" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G21(){
+    document.getElementById("md21").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16364" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G22(){
+    document.getElementById("md22").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16784" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G23(){
+    document.getElementById("md23").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16786" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G24(){
+    document.getElementById("md24").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16788" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G25(){
+    document.getElementById("md25").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16829" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G26(){
+    document.getElementById("md26").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16831" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G27(){
+    document.getElementById("md27").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16832" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G28(){
+    document.getElementById("md28").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16834" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G29(){
+    document.getElementById("md29").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16836" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G30(){
+    document.getElementById("md30").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16838" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G31(){
+    document.getElementById("md31").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16840" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G32(){
+    document.getElementById("md32").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16842" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G33(){
+    document.getElementById("md33").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16846" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G34(){
+    document.getElementById("md34").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16844" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G35(){
+    document.getElementById("md35").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16850" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G36(){
+    document.getElementById("md36").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16848" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G37(){
+    document.getElementById("md37").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16851" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G38(){
+    document.getElementById("md38").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16853" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G39(){
+    document.getElementById("md39").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16855" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G40(){
+    document.getElementById("md40").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16857" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G41(){
+    document.getElementById("md41").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16859" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G42(){
+    document.getElementById("md42").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16861" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G43(){
+    document.getElementById("md43").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16865" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G44(){
+    document.getElementById("md44").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16863" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G45(){
+    document.getElementById("md45").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16867" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G46(){
+    document.getElementById("md46").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16871" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G47(){
+    document.getElementById("md47").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16874" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G48(){
+    document.getElementById("md48").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16877" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G49(){
+    document.getElementById("md49").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16879" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G50(){
+    document.getElementById("md50").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16880" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G51(){
+    document.getElementById("md51").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16882" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G52(){
+    document.getElementById("md52").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16884" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G53(){
+    document.getElementById("md53").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16887" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G54(){
+    document.getElementById("md54").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16889" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G55(){
+    document.getElementById("md55").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16891" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G56(){
+    document.getElementById("md56").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16893" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G57(){
+    document.getElementById("md57").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16895" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G58(){
+    document.getElementById("md58").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16897" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G59(){
+    document.getElementById("md59").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16903" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G60(){
+    document.getElementById("md60").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16905" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G61(){
+    document.getElementById("md61").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16907" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
+
+function G62(){
+    document.getElementById("md62").innerHTML='<div class="video-container"><iframe src="https://www.footbie.com/es/embed/16910" frameborder="0" width="640" height="360" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen allowtransparency></iframe></div>'}
